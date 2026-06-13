0057f90f1f4c017a6ba38a87a27006ebf9e84c64
ianclov <iansteveg@gmail.com>
2026-06-13 10:15:42 -0700

    baal movement and loose finished

--- pingpomg.py
+++ pingpomg.py
@@ -1,12 +1,12 @@
 from pygame import *
-'''Required classes'''
+'''Clases requeridas'''
 
 
 #clase padre para los objetos
 class GameSprite(sprite.Sprite):
    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
        super().__init__()
-       self.image = transform.scale(image.load(player_image), (wight, height)) #por ejemplo. 55,55 - parámetros
+       self.image = transform.scale(image.load(player_image), (wight, height)) #es decir. 55,55 - parámetros
        self.speed = player_speed
        self.rect = self.image.get_rect()
        self.rect.x = player_x
@@ -15,8 +15,6 @@ class GameSprite(sprite.Sprite):
 
    def reset(self):
        window.blit(self.image, (self.rect.x, self.rect.y))
-
-
 class Player(GameSprite):
    def update_r(self):
        keys = key.get_pressed()
@@ -24,15 +22,13 @@ class Player(GameSprite):
            self.rect.y -= self.speed
        if keys[K_DOWN] and self.rect.y < win_height - 80:
            self.rect.y += self.speed
-def update_l(self):
+   def update_l(self):
        keys = key.get_pressed()
        if keys[K_w] and self.rect.y > 5:
            self.rect.y -= self.speed
        if keys[K_s] and self.rect.y < win_height - 80:
            self.rect.y += self.speed
-
-
-#escena del videojuego:
+#escena del juego:
 back = (200, 255, 255) #цвет фона (fondo)
 win_width = 600
 win_height = 500
@@ -51,12 +47,12 @@ FPS = 60
 racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
-
-
 font.init()
 font = font.Font(None, 35)
-lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
-lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+lose1 = font.render('¡EL JUGADOR 1 PIERDE!', True, (180, 0, 0))
+lose2 = font.render('EL JUGADOR 2 PIERDE!', True, (180, 0, 0))
+
+
 speed_x = 3
 speed_y = 3
 
@@ -72,25 +68,22 @@ while game:
        racket2.update_r()
        ball.rect.x += speed_x
        ball.rect.y += speed_y
-
-
-       if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
            speed_x *= -1
            speed_y *= 1
-#si la pelota llega a los bordes de la pantalla, cambiar dirección de movimiento
+      
+       #si la pelota alcanza los bordes de la pantalla, cambiar la dirección del movimiento
        if ball.rect.y > win_height-50 or ball.rect.y < 0:
            speed_y *= -1
 
 
-       #si la pelota va más allá de esta paleta, mostrar la condición de derrota para el jugador 1
+       #si la pelota va más allá de la paleta, mostrar la condición de derrota para el jugador 1
        if ball.rect.x < 0:
            finish = True
            window.blit(lose1, (200, 200))
            game_over = True
-
-
-       #si la pelota va más allá de esta paleta, mostrar la condición de derrota para el jugador 2
-if ball.rect.x > win_width:
+#si la pelota va más allá de la paleta, mostrar la condición de derrota para el jugador 2
+       if ball.rect.x > win_width:
            finish = True
            window.blit(lose2, (200, 200))
            game_over = True
